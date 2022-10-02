53c93ce5c8efe3c34baf593c4b1ccb50b781ca6c
Felipe Portugal <felipetpramirez@gmail.com>
2022-10-02 14:36:57 -0300

    Revert "alterações 02-10"
    This reverts commit fc831bc3776ca3df3e5efe94c9054d015e724151.

--- src/routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const controller = require("../controllers/userController");
-
-router.get("/all", controller.getAll);
-
-module.exports = router;
