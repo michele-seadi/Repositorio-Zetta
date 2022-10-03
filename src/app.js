c84828e9b5c7886863560f838e1b49d36f7666e3
Felipe Portugal <felipetpramirez@gmail.com>
2022-10-03 14:23:32 -0300

    Autenticação

--- src/app.js
+++ src/app.js
@@ -1,15 +1,17 @@
-const express = require('express');
-const app = express();
-const cors = require('cors');
+const express = require('express')
+const app = express()
+const cors = require('cors')
 
 require('dotenv-safe').config();
 
-const db = require('./config/database');
-const userRoutes = require('./routes/userRoutes');
+const db = require('./config/database')
+const userRoutes = require('./routes/userRoutes')
 
-db.connect();
-app.use(cors());
-app.use(express.json());
-app.use("./users", userRoutes);
 
-module.exports = app;
+db.connect() 
+
+app.use(cors())
+app.use(express.json())
+app.use("/users", userRoutes)
+
+module.exports = app
